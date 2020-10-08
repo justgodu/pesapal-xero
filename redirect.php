05bac8a17e3e05c052f1c7fcdb1d5f50ce5cfc9b
justgodu <nikkagoduadze@gmail.com>
2020-10-08 09:51:27 +0400

    First Commit

---
+++ redirect.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesapal-Xero</title>
+</head>
+<body>
+<div style="margin:auto; max-iwdth:200px background-color:#c1c1c1">
+    <h1>Redirect</h1>
+    <a href="http://localhost:8080">Home</a>
+    </div>
+</body>
+</html>
